473dd29da4cdcf2ba9ecc0f07dd8414664ad36fe
Mahir Nanjib Chowdhury <mahirnanjib166@gmail.com>
2024-03-05 23:44:15 +0600

    have to add event listner

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-
+// finding Elements from html
+let mainContainer = document.querySelector(".main-container")
+let todoContainer = document.querySelector(".todo-container")
+let todoForm = document.querySelector(".todo-form")
+let inputBox = document.querySelector(".input-box")
+let button = document.querySelector(".btn");
